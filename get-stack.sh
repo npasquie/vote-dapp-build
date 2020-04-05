e4ee23496d2d9dca2b7189694d8a52328263100b
npasquie <nicolas.pasquier.pro@gmail.com>
2020-04-05 19:22:11 +0200

    contract project added

--- get-stack.sh
+++ get-stack.sh
@@ -7,3 +7,8 @@ if [ ! -d "./vote-dapp-back/vote-dapp-front/" ]
 then
   git clone https://github.com/npasquie/vote-dapp-front.git
 fi
+
+if [ ! -d "./vote-dapp-back/vote-dapp-front/vote-dapp-contract" ]
+then
+  git clone https://github.com/npasquie/vote-dapp-contract.git
+fi
