88cd0af296fa6e386a27f6d9302e3eea6016adb9
npasquie <nicolas.pasquier.pro@gmail.com>
2020-04-29 12:14:02 +0200

    get stack rectif

--- get-stack.sh
+++ get-stack.sh
@@ -8,7 +8,7 @@ then
   git clone https://github.com/npasquie/vote-dapp-front.git ./vote-dapp-back/
 fi
 
-if [ ! -d "./vote-dapp-back/vote-dapp-front/vote-dapp-contract" ]
+if [ ! -d "./vote-dapp-back/vote-dapp-front/vote-dapp-contract/" ]
 then
   git clone https://github.com/npasquie/vote-dapp-contract.git ./vote-dapp-back/vote-dapp-front/
 fi
