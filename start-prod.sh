a84b9f4622ee3966cec621510410852980f69f07
npasquie <nicolas.pasquier.pro@gmail.com>
2020-04-29 12:26:46 +0200

    start prod rectif

--- start-prod.sh
+++ start-prod.sh
@@ -1,8 +1,8 @@
 sh ./get-stack.sh
 if [ ! -d "./vote-dapp-back/vote-dapp-front/build/" ]
 then
-  echo "you need to go in front project and build it (yarn build)"
-else
-  docker-compose -f docker-compose.yml build
-  docker-compose -f docker-compose.yml up
+  yarn --cwd ./vote-dapp-back/vote-dapp-front/ install
+  yarn --cwd ./vote-dapp-back/vote-dapp-front/ build
 fi
+docker-compose -f docker-compose.yml build
+docker-compose -f docker-compose.yml up
