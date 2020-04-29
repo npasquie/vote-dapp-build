0417ac3a662942917d512074c52c5eae21432a8f
npasquie <nicolas.pasquier.pro@gmail.com>
2020-04-29 12:22:41 +0200

    start prod rectif

--- start-prod.sh
+++ start-prod.sh
@@ -1,7 +1,9 @@
 sh ./get-stack.sh
 if [ ! -d "./vote-dapp-back/vote-dapp-front/build/" ]
 then
+  cd ./vote-dapp-back/vote-dapp-front/
   yarn --cwd ./vote-dapp-back/vote-dapp-front/ build
+  cd ../../
 fi
 docker-compose -f docker-compose.yml build
 docker-compose -f docker-compose.yml up
